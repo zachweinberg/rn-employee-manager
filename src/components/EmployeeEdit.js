2b2d702dd27f124cc5b3ae48b20802a93866b6b7
Zach James <zjwgibsg@gmail.com>
2018-09-22 00:27:36 -0400

    added fire action

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDete } from '../actions';
 import { CardBox, CardSection, Button, Confirm } from './common';
 import Communications from 'react-native-communications';
 
 class EmployeeEdit extends Component {
+
+  state = { showModal: false}
 
   componentDidMount() {
     _.each(this.props.employee, (value, prop) => {
@@ -23,6 +26,15 @@ class EmployeeEdit extends Component {
   onTextPress = () => {
     const { phone, shift } = this.props;
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
+  onAccept = () => {
+    const { uid } = this.props.employee;
+    this.props.employeeDelete({ uid });
+  }
+
+  onDecline = () => {
+    this.setState({ showModal: false });
   }
 
 
@@ -44,11 +56,18 @@ class EmployeeEdit extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal})}>
             Fire Employee
           </Button>
         </CardSection>
 
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept}
+          onDecline={this.onDecline}
+        >
+          Are you sure you want to delete this user?
+        </Confirm>
       </CardBox>
     );
   }
@@ -60,4 +79,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -1,4 +1,48 @@
-import React, { Component } from 'react-native';
-import { Text, View, Modal } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Modal, StyleSheet } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
+
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      onRequestClose={ () => {} }
+      transparent
+    >
+      <View style={styles.containerStyle}>
+        <CardSection style={styles.cardSectionStyle}>
+          <Text style={styles.textStyle}>{children}</Text>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0,0,0,0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
+});
+
+
+export { Confirm };
